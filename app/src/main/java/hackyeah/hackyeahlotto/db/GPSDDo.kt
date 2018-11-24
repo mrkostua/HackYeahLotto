e06597d4bb1d0c9c5f435744f849847f345a3bf5
mrkostua <kprysiazhnyi@gmail.com>
2018-11-24 23:49:26 +0100

    todo

--- app/src/main/java/hackyeah/hackyeahlotto/db/GPSDDo.kt
+++ app/src/main/java/hackyeah/hackyeahlotto/db/GPSDDo.kt
@@ -8,7 +8,7 @@ import java.util.*
  * @author Kostiantyn Prysiazhnyi on 11/24/2018.
  */
 @Entity(tableName = "gps")
-data class GPSDDo(
+data class GPSDDo( //TODO what to save , how to save time ?????
     @PrimaryKey(autoGenerate = true) var id: Int? = null, var latitutde: Double = 0.0,
     var longitude: Double = 0.0,
     var date: Date?)
