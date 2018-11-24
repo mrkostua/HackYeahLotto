c7df6eca9d0d4a1e2157162d8e9273377c7ab069
Bartosz Wołcerz <bwolcerz@corelogic.pl>
2018-11-24 22:11:12 +0100

    widok stan konta

--- app/src/main/java/hackyeah/hackyeahlotto/AllFeaturesActivity.java
+++ app/src/main/java/hackyeah/hackyeahlotto/AllFeaturesActivity.java
@@ -1,13 +1,32 @@
 package hackyeah.hackyeahlotto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AllFeaturesActivity extends AppCompatActivity {
+    private Button stateActivityBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_features);
+        stateActivityBtn = findViewById(R.id.state_activity_btn);
+        stateActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runStateActivity();
+            }
+        });
     }
+    public void runStateActivity() {
+        Intent activity = new Intent(this, StateActivity.class);
+        startActivity(activity);
+    }
+
+
+
 }
--- app/src/main/java/hackyeah/hackyeahlotto/StateActivity.java
+++ app/src/main/java/hackyeah/hackyeahlotto/StateActivity.java
@@ -5,16 +5,29 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * @author Kostiantyn Prysiazhnyi on 11/24/2018.
- */
+
 public class StateActivity extends AppCompatActivity {
+
+    private Button buyTicketBtn;
+    private Button bonusBtn;
+    private Button accountState;
+    private TextView nextDrawTv;
+    private Button seeResultBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_state);
+        buyTicketBtn = findViewById(R.id.buy_ticket_btn);
+        bonusBtn = findViewById(R.id.daily_bonus_btn);
+        accountState = findViewById(R.id.point_state_btn);
+        seeResultBtn = findViewById(R.id.see_results_btn);
+        nextDrawTv = findViewById(R.id.next_draw_tv);
     }
+
 
     public void loginClickListener(View view) {
         Intent intentAllFeaturesActiivity = new Intent(this, AllFeaturesActivity.class);
