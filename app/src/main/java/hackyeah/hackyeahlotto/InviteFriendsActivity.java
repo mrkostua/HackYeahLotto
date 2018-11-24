653ba7a67bafa6e5c0b67345b3e462d8b42fdcf2
Patrick Täsler <p.taesler@gmx.de>
2018-11-25 00:50:30 +0100

    remove back arrow

--- app/src/main/java/hackyeah/hackyeahlotto/InviteFriendsActivity.java
+++ app/src/main/java/hackyeah/hackyeahlotto/InviteFriendsActivity.java
@@ -2,10 +2,8 @@ package hackyeah.hackyeahlotto;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
-import java.util.Objects;
 /**
  * @author Patrick Täsler on 11/24/2018.
  */
@@ -17,15 +15,5 @@ public class InviteFriendsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_friends);
         image = findViewById(R.id.imageViewInviteFacebookMessenger);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-    }
-
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            finish();
-        }
-        return true;
     }
 }
